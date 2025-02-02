e76ff2c41314e53202d378a7e6d027313c755da8
Ateeb Sohail <49984737+ateebplayz@users.noreply.github.com>
2025-02-02 12:05:52 +0500

    Include out dir

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  transpilePackages: ['three']
+  transpilePackages: ['three'],
+  output: 'export'
 };
 
 export default nextConfig;
